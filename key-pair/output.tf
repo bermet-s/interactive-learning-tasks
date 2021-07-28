e92ddb06be95798d7a8aba96a8e47e87d793a1db
bermet-s <bermet.shabaeva@gmail.com>
2021-07-28 22:08:15 +0000

    edited files

--- key-pair/output.tf
+++ key-pair/output.tf
@@ -3,10 +3,10 @@ output "region" {
 }
 
 output "key_name" {
-  value = "ilearning-wordpress"
+  value = aws_key_pair.ilearning-wordpress.key_name
   }
 
 output "key_id" {
-  value = "key-0e369965d651c2d0c"
+  value = aws_key_pair.ilearning-wordpress.id
 }
 
