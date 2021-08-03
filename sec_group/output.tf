eca8e9937dd4f8f3882c9377f41353d5bdd5a42a
bermet-s <bermet.shabaeva@gmail.com>
2021-08-03 21:30:12 +0000

    edited the files

--- sec_group/output.tf
+++ sec_group/output.tf
@@ -1,15 +1,15 @@
 output "sec_group_id" {
-value = aws_security_group.external_by_terraform.id
+  value = aws_security_group.external_by_terraform.id
 }
 
 output "owner_id" {
-value = aws_security_group.external_by_terraform.owner_id
+  value = aws_security_group.external_by_terraform.owner_id
 }
 
 output "arn" {
-value = aws_security_group.external_by_terraform.arn
+  value = aws_security_group.external_by_terraform.arn
 }
 
 output "region" {
-value = "us-east-1"
+  value = "us-east-1"
 }
--- sec_group/sec_group.tf
+++ sec_group/sec_group.tf
@@ -1,32 +1,32 @@
 resource "aws_security_group" "external_by_terraform" {
-  name        = "external_by_terraform"
+  name        = "allow_inbound_traffic"
   ingress {
     description      = "TLS from VPC"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [50.194.68.230/32]
+    cidr_blocks      = ["50.194.68.230/32"]
   }
   ingress {
     description      = "TLS from VPC"
     from_port        = 3306
     to_port          = 3306
     protocol         = "tcp"
-    cidr_blocks      = [50.194.68.231/32]
+    cidr_blocks      = ["50.194.68.231/32"]
   }
   ingress {
     description      = "TLS from VPC"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
   ingress {
     description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
   egress {
     from_port        = 0
@@ -34,3 +34,4 @@ resource "aws_security_group" "external_by_terraform" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+}
